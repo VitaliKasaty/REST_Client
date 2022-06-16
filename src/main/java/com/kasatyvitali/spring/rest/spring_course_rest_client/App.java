6db8276c28aedf89870c8c35d3d698554290cf37
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-16 21:12:37 +0300

    Create app

---
+++ src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/App.java
@@ -0,0 +1,13 @@
+package com.kasatyvitali.spring.rest.spring_course_rest_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/configuration/MyConfig.java
@@ -0,0 +1,5 @@
+package com.kasatyvitali.spring.rest.spring_course_rest_client.configuration;
+
+public class MyConfig {
+
+}
