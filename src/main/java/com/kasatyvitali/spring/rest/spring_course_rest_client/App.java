3c775b49599fbb7c1a9f06d9ab222320445a2e70
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-16 21:55:44 +0300

    Add CRUD operations to rest_client app

--- src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/App.java
+++ src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/App.java
@@ -1,13 +1,35 @@
 package com.kasatyvitali.spring.rest.spring_course_rest_client;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kasatyvitali.spring.rest.Communication;
+import com.kasatyvitali.spring.rest.entity.Employee;
+import com.kasatyvitali.spring.rest.spring_course_rest_client.configuration.MyConfig;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context = 
+        		new AnnotationConfigApplicationContext(MyConfig.class);
+        
+        Communication communication =context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+        
+        Employee empById = communication.getEmployee(10);
+        System.out.println(empById);
+        
+        Employee emp = new Employee("Sveta", "Sokolova", "HR", 900);        
+        communication.saveEmployee(emp);
+        
+        Employee emp1 = new Employee("Sveta", "Sokolova", "IT", 1900);
+        emp.setId(12);
+        communication.saveEmployee(emp1);
+        
+        communication.deleteEmployee(12);
+        
     }
 }
--- src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/configuration/MyConfig.java
+++ src/main/java/com/kasatyvitali/spring/rest/spring_course_rest_client/configuration/MyConfig.java
@@ -1,5 +1,16 @@
 package com.kasatyvitali.spring.rest.spring_course_rest_client.configuration;
 
-public class MyConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+@Configuration
+@ComponentScan("com.kasatyvitali.spring.rest")
+public class MyConfig {
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
